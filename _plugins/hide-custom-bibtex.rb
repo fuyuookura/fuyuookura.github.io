3f121f923a57a0a671d34220adbaf2badcc6892e
fuyuookura <fuyuookura@163.com>
2025-11-18 19:11:59 -0500

    add

--- _plugins/hide-custom-bibtex.rb
+++ _plugins/hide-custom-bibtex.rb
@@ -3,6 +3,9 @@ module Jekyll
     def hideCustomBibtex(input)
 	    keywords = @context.registers[:site].config['filtered_bibtex_keywords']
 
+      # Return input unchanged if filtered_bibtex_keywords is not configured
+      return input if keywords.nil? || keywords.empty?
+
 	    keywords.each do |keyword|
 		    input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
 	    end
