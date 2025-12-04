ca75fd1e684f262c12ea8391fd5b4206b4dbdd6a
fuyuookura <133614528+fuyuookura@users.noreply.github.com>
2025-12-04 00:31:43 +0000

    Deploying to gh-pages from @ fuyuookura/fuyuookura.github.io@721a6dd42f0d75317432aadcc5bcc0a2512fd4ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,9 +39,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-lenovo",
+          title: 'Lenovo',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
